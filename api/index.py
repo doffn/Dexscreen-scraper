e07e76e83d8b06cf63ebaa6d024558bf9fa2183d
doffn <80069784+doffn@users.noreply.github.com>
2024-04-19 02:31:40 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler
+import telebot
 
 class handler(BaseHTTPRequestHandler):
 
@@ -6,5 +7,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
+        bot = telebot.TeleBot("6227128550:AAGTVw8ERRRAdZK0kSaRqXLkEXeloIXGg_0")
+        bot.send_message(chat_id="-1001802310005", text="Hi there bitch it worked")
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return
