edf04d0f69bf73574f045f8fbc2947b61276d2f1
doffn <80069784+doffn@users.noreply.github.com>
2024-04-20 14:36:42 +0300

    Update main_code.py

--- api/main_code.py
+++ api/main_code.py
@@ -20,6 +20,8 @@ import os
 import sys
 from tweety import Twitter
 from telebot import formatting
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 import shutil
 import json
@@ -31,6 +33,7 @@ ID = "-1001802310005"
 bot = telebot.TeleBot(API)
 bot2 = telebot.TeleBot('5649885726:AAE1JFJlvrbqXE6d8ww-LKoq54wO5OpjNYY')
 chat_id = "-1001707169481"
+URL = "mongodb+srv://doffneri:Jkll2okBSwvKOLaQ@cluster0.g3hk4tc.mongodb.net/?retryWrites=true&w=majority"
 
 def send_group_message(message):
     try:
@@ -56,24 +59,6 @@ def fix_json():
           shutil.copy2('data_backup.json', 'data.json')
 
 
-def get_mongo():
-    with open('data.json', 'r+') as file:
-        try:
-            data = json.load(file)
-            return data
-        except json.decoder.JSONDecodeError:
-            print("There is a JSON error")
-
-        finally:
-            fix_json()
-
-
-
-def mongo_update(data):
-      with open('data.json', 'w') as file:
-            json.dump(data, file, indent=4)
-
-
 def report(message, channel_id=ID):
     try:
         bot.send_message(channel_id, message, parse_mode='MarkdownV2')
@@ -81,6 +66,68 @@ def report(message, channel_id=ID):
     except Exception as e:
         print(e)
         return False
+        
+def get_mongo():
+    client = MongoClient(URL, server_api=ServerApi('1'))
+    try:
+        # Access the database and collection
+        db = client['Tweet_tg']
+        collection = db['my_first_data']
+
+        # Retrieve a single document from the collection based on the query
+        document = collection.find_one()
+        report(document[:30])
+        return document
+
+    except Exception as e:
+        print(f"Error connecting to MongoDB: {e}")
+        return None
+
+    finally:
+        # Close the connection in a finally block to ensure it is always closed
+        client.close()
+
+
+
+def mongo_update(files, remove=False, set_empty=False):
+    """
+    files: dict, Json ; defines the json or dictionary to be updated
+    remove: str ; defines the keys to be removed. If you want to remove a key inside a key, separate the keys with a dot.
+    set_empty: bool ; will remove all data in the collection
+    """
+    try:
+        client = MongoClient(URL, server_api=ServerApi('1'))
+        db = client['Tweet_tg']
+        collection = db['my_first_data']
+        document = collection.find_one()
+
+        # Exclude '_id' field from the update query
+        files_without_id = files.copy()
+        files_without_id.pop('_id', None)
+
+        if remove:
+            keys = remove.split('.')
+            nested_dict = data
+            for key in keys[:-1]:
+                nested_dict = nested_dict[key]
+            del nested_dict[keys[-1]]
+        if set_empty:
+            for key in files_without_id:
+                collection.update_one({"_id": document["_id"]}, {"$unset": {key: ""}})
+        else:
+            update_query = {"$set": files_without_id}
+            collection.update_one({"_id": document["_id"]}, update_query)
+
+        # Return the modified files dictionary
+        return files_without_id
+    except Exception as e:
+        print(f"Error updating document: {e}")
+    finally:
+        client.close()
+
+
+
+
 
 
 
