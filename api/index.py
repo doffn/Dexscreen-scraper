7c282185f41f819e47d216d2130efc0bb5963d81
doffn <80069784+doffn@users.noreply.github.com>
2024-04-21 03:01:25 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,7 +10,6 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         try:
             bot = telebot.TeleBot("6098595418:AAFpqdwwoMXPPv8VxroKZIejJ5LYky8rtGY")
-            life()
             #bot.send_message(chat_id="-1001873201570", text="Hi there bitch it worked")
         except Exception as e:
             print(e)
