dc00764b8c7c9a8606e11f5fecbc1d6cb24df529
doffn <80069784+doffn@users.noreply.github.com>
2024-04-21 18:23:12 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,33 +2,27 @@ from flask import Flask, request
 from api.main_code import *
 import telebot
 
-
-from flask import Flask, request
-import telebot
-
 app = Flask(__name__)
 bot = telebot.TeleBot("YOUR_TOKEN")
 
 @app.route('/', methods=['GET'])
 def root():
-    reviewer()
-    return 'Hello, world! This is the root endpoint.'
+    return '<body style="background-color:black; color:white;">Hello, world! This is the root endpoint.</body>'
 
 @app.route('/service', methods=['GET', 'POST'])
 def service():
+    main_function()
     if request.method == 'POST':
         try:
             # Code for the '/service' POST endpoint
             # Perform any necessary actions or computations
-            main_function()
-            return 'Service endpoint accessed'
+            
+            return '<body style="background-color:black; color:white;">Service endpoint accessed</body>'
         except Exception as e:
             print(e)
-            return 'An error occurred'
+            return '<body style="background-color:black; color:white;">An error occurred</body>'
     else:
-        return 'Service endpoint accessed'
-
-
+        return '<body style="background-color:black; color:white;">Service endpoint accessed</body>'
 
 if __name__ == '__main__':
     app.run()
