fabd9680420cf6fd354c96b17d7fd79a4c2a51fe
doffn <80069784+doffn@users.noreply.github.com>
2024-04-21 21:19:52 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,23 +1,27 @@
 from flask import Flask, request
 from api.main_code import *
 import threading
+from concurrent.futures import ThreadPoolExecutor
 import telebot
 app = Flask(__name__)
 bot = telebot.TeleBot("YOUR_TOKEN")
 
 
-    
+executor = ThreadPoolExecutor()
+
 @app.route('/', methods=['GET'])
 def root():
     return '<body style="background-color:black; color:white; font-family: Arial, sans-serif;">Hello User, This is my API</body>'
 
 @app.route('/service', methods=['GET', 'POST'])
 def service():
-
+    """
     thread1 = threading.Thread(target=main_function)
     thread1.setDaemon(True)
     thread1.join()
-    thread1.start()
+    thread1.start()"""
+    # Submit the task to the thread pool for asynchronous execution
+    executor.submit(run_main_function)
 
     if request.method == 'POST':
         try:
@@ -30,6 +34,11 @@ def service():
             return '<body style="background-color:black; color:white;">An error occurred</body>'
     else:
         return '<body style="background-color:black; color:white;">Service endpoint accessed</body>'
+
+def run_main_function():
+    # Simulate a time-consuming task
+    main_function()
+
 
 if __name__ == '__main__':
     app.run()
