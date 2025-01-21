d6aaddf69629e8e0c1e85e4189edf38368fa4932
Dawit Neri <80069784+doffn@users.noreply.github.com>
2025-01-22 00:09:38 +0300

    new

--- api/index.py
+++ api/index.py
@@ -73,7 +73,7 @@ def ui():
         # Fetch the token data
         new = DexBot(Api, ID, chain=False)
         mes = new.format_token_data()  # This will connect and fetch the token data
-        tokens = json.loads(json.dumps(mes)).get("data")  # Parse JSON data into a Python object
+        tokens = json.dumps(mes).get("data")  # Parse JSON data into a Python object
 
         print(len(tokens))
 
